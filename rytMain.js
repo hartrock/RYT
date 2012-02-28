848e4b81fe27dea459c41b0aff92d6f728e34afb
Stephan Rudlof <sr@evolgo.de>
2012-02-28 04:05:30 +0100

    comment corrected

--- rytMain.js
+++ rytMain.js
@@ -26,7 +26,7 @@ var EvolGo = EvolGo || {}, RYT = RYT || {};
 (function (eg, ryt) {
 function window_onload () {
 
-  var version = "RYTv2.2.1 (2012-02-28)"; // programmatically updated from updateVersion
+  var version = "RYTv2.2.1 (2012-02-28)"; // programmatically updated from upgradeVersion
 
   var eg = EvolGo, ryt = RYT; // shortcuts
 
