1c821f5d630378ae1017473fd13a61da45792744
Stephan Rudlof <sr@evolgo.de>
2024-02-05 00:15:17 +0100

    Scripts/upgradeVersion v2.2.7.5.4

--- rytMain.js
+++ rytMain.js
@@ -26,7 +26,7 @@ var EvolGo = EvolGo || {}, RYT = RYT || {};
 (function (eg, ryt) {
 function window_onload () {
 
-  var version = "RYTv2.2.7.5.3 (2017-12-14)"; // programmatically updated from upgradeVersion
+  var version = "RYTv2.2.7.5.4 (2024-02-05)"; // programmatically updated from upgradeVersion
 
   var eg = EvolGo, ryt = RYT; // shortcuts
 
